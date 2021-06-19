d4efe58f713c59959e0000bbe0cf456ea37c336d
edward evbert <edwardevbert@gmail.com>
2021-06-20 01:39:12 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,16 +9,10 @@ class AppServiceProvider extends ServiceProvider
 
 	public function register()
 	{
-		Validator::extend('alpha_spaces', function ($attribute, $value) {
-  // This will only accept alpha and spaces. 
- 	// If you want to accept hyphens use: /^[\pL\s-]+$/u.
-			return preg_match('/^[\pL\s]+$/u', $value); 
-		});
-		$request = app('request');
-
-		if ($request->isMethod('OPTIONS'))
-		{
-			app()->options($request->path(), function() { return response('', 200); });
-		}
+		// Validator::extend('alpha_spaces', function ($attribute, $value) {
+  // // This will only accept alpha and spaces. 
+ 	// // If you want to accept hyphens use: /^[\pL\s-]+$/u.
+		// 	return preg_match('/^[\pL\s]+$/u', $value); 
+		// });
 	}
 }
